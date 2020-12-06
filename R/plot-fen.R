2ab9d448f775f8a1b112bf80abfcadbec7fbdf0a
holmes <c.holmes4@gmail.com>
2020-12-06 13:58:28 +0000

    Add starting position as default FEN. Add piece attribution to documentation

--- R/plot-fen.R
+++ R/plot-fen.R
@@ -1,16 +1,24 @@
 #' Visualise a FEN
 #'
-#' @param fen a character vector FEN of length one
+#' @param fen a character vector FEN of length one (defaults to the opening position)
 #' @param piece_scale scaling factor for piece sizes (default = 0.75)
 #' @param perspective view board from white "w" or black "b" perspective (default = "w")
 #' @param cols Length 2 vector giving colours of dark and light squares (default = c("#b58863ff", "#f0d9b5ff"))
+#'
+#' @details Chess piece SVG design file downloaded from https://commons.wikimedia.org/wiki/File:Chess_Pieces_Sprite.svg
+#'
+#'  jurgenwesterhof (adapted from work of Cburnett), CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0>, via Wikimedia Commons
+#'
 #'
 #' @return A ggplot2 plot object
 #' @export
 #'
 #' @examples
 #' plot_fen('Q1b2rk1/p1p2p1p/6Bp/2b5/8/2P5/P1P2PPP/R4RK1 b - - 0 16')
-plot_fen <- function(fen, perspective = "w", cols = c("#b58863ff", "#f0d9b5ff"), piece_scale = 0.85){
+plot_fen <- function(fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+                     perspective = "w",
+                     cols = c("#b58863ff", "#f0d9b5ff"),
+                     piece_scale = 0.85){
 
   # Convert FEN to a df, join to only the relevant piece polygons (inner join)
   # Adjust the pieces to sit in the right squares (and apply scaling to piece size)
