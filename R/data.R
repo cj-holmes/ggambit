dbbe9d4c4998b90156fc0532bed8bc90d56fb35f
holmes <c.holmes4@gmail.com>
2020-12-07 17:17:48 +0000

    Overhaul of plotting (to add options to remove coordinates and FEN caption) and also FEN parsing with a notation_lookup vector

--- R/data.R
+++ R/data.R
@@ -1,9 +1,20 @@
 #' The chess piece polygon paths
 #'
+#' Not inteded for use by the user
+#'
 #' @format A dataframe of chess piece polygon coordinates
 "paths"
 
 #' A list of dark and light square colour options
+#'
+#' Not inteded for use by the user
+#'
+#' @format A named list of dark and light square colours
+"colour_lookup"
+
+#' A named vector for help when parsing FEN strings
+#'
+#' Not inteded for use by the user
 #'
 #' @format A named list of dark and light square colours
 "colour_lookup"
