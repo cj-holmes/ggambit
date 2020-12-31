4b71f97c0b4609b55f9d3c76b1d68041e90d9d50
holmes <c.holmes4@gmail.com>
2020-12-31 19:29:34 +0000

    Document data

--- R/data.R
+++ R/data.R
@@ -16,5 +16,19 @@
 #'
 #' Not intended for use by the user
 #'
-#' @format A named list of dark and light square colours
+#' @format A named vector
 "notation_lookup"
+
+#' A named vector for help when plotting chess positions
+#'
+#' Not intended for use by the user
+#'
+#' @format A named vector
+"square_lookup"
+
+#' A tibble containing the PGN seven tag roster
+#'
+#' Not intended for use by the user
+#'
+#' @format A tibble
+"seven_tag_roster"
